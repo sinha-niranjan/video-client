670515829f0493f712d07bf63f38c93dce362b1a
Niranjan Kumar <niranjankumaraina@gmail.com>
2024-03-17 08:38:48 +0530

    Changes to be committed: 	modified:   src/context/SocketProvider.jsx

--- src/context/SocketProvider.jsx
+++ src/context/SocketProvider.jsx
@@ -8,9 +8,10 @@ export const useSocket = () => {
 };
 
 const url = "localhost:8000";
+// "https://vidoe-call-server.vercel.app/"
 
 export const SocketProvider = (props) => {
-  const socket = useMemo(() => io("https://vidoe-call-server.vercel.app/"), []);
+  const socket = useMemo(() => io(url), []);
 
   return (
     <socketContext.Provider value={socket}>
